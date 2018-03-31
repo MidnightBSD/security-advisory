1b8ebf0a242305bac8d575b220afdb2d2f867527
Lucas Holt <luke@foolishgames.com>
2018-03-31 19:59:42 -0400

    add vendor controller

---
+++ src/main/java/org/midnightbsd/advisory/ctl/api/VendorController.java
@@ -0,0 +1,39 @@
+package org.midnightbsd.advisory.ctl.api;
+
+import org.midnightbsd.advisory.model.Vendor;
+import org.midnightbsd.advisory.repository.VendorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Lucas Holt
+ */
+@RestController
+@RequestMapping("/api/vendor")
+public class VendorController {
+
+    @Autowired
+    private VendorRepository vendorRepository;
+
+    @GetMapping
+    public ResponseEntity<Page<Vendor>> list(Pageable page) {
+        return ResponseEntity.ok(vendorRepository.findAll(page));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vendor> get(@PathVariable("id") int id) {
+        return ResponseEntity.ok(vendorRepository.findOne(id));
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Vendor> get(@PathVariable("name") String name) {
+        return ResponseEntity.ok(vendorRepository.findOneByName(name));
+    }
+
+}
