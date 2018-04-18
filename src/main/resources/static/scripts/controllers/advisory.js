6c00fa4a17dcafdcd773e244a336872266e4be80
Lucas Holt <luke@foolishgames.com>
2018-04-18 09:49:29 -0400

    fix several bugs

--- src/main/resources/static/scripts/controllers/advisory.js
+++ src/main/resources/static/scripts/controllers/advisory.js
@@ -2,26 +2,5 @@ angular.module('wwwApp').controller('AdvisoryCtrl', ['$scope', '$routeParams', '
     function ($scope, $routeParams, $location, $http, AdvisoryService) {
         'use strict';
 
-        $scope.max = 5;
-        $scope.size = 50;
-        $scope.ratings = {};
-
-        $scope.page = $location.search().page;
-        if (typeof $scope.page === 'undefined')
-            $scope.page = 0;
-
-
-        $scope.advisories = AdvisoryService.queryByVendor({ Name: $routeParams.vendor});
-
-        $scope.load = function() {
-            $scope.advisories = AdvisoryService.queryByVendor({
-                vendor: $routeParams.vendor,
-                page: $scope.page,
-                size: $scope.size
-            }, function(pkgs) {
-          //      $scope.totalElements = $scope.advisories.totalElements;
-            });
-        };
-
-        $scope.load();
+        $scope.advisories = AdvisoryService.queryByVendorName({vendor: $routeParams.vendor});
     }]);
--- src/main/resources/static/scripts/controllers/main.js
+++ src/main/resources/static/scripts/controllers/main.js
@@ -1,12 +1,29 @@
-angular.module('wwwApp').controller('MainCtrl', ['$scope', '$http', '$location', 'AdvisoryService', 'VendorService',
-    function ($scope, $http, $location, AdvisoryService, VendorService) {
-    'use strict';
+angular.module('wwwApp').controller('MainCtrl', ['$scope', '$http', '$location', 'VendorService',
+    function ($scope, $http, $location, VendorService) {
+        'use strict';
 
-    $scope.advisories = AdvisoryService.query();
+        $scope.vendors = [];
 
-    $scope.vendors = VendorService.getPaged({page: 0});
 
-    $scope.search = function() {
-        $location.path('/search').search('keyword', $scope.term);
-    }
-}]);
+        $scope.load = function () {
+            var vendor = VendorService.getPaged({page: 0, size: 100}, function () {
+                for (var x = 0; x < vendor.content.length; x++) {
+                    $scope.vendors.push(vendor.content[x]);
+                }
+
+                for (var i = 1; i < vendor.totalPages; i++) {
+                      VendorService.getPaged({page: i, size: 100}, function (result) {
+                          for (var z = 0; z < result.content.length; z++) {
+                              $scope.vendors.push(result.content[z]);
+                          }
+                    });
+                }
+            });
+        };
+
+        $scope.search = function () {
+            $location.path('/search').search('keyword', $scope.term);
+        };
+
+        $scope.load();
+    }]);
