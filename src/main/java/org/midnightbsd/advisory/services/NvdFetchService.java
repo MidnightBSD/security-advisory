6d421fb82345e893be0f588aaf61df1444e737d9
Lucas Holt <luke@foolishgames.com>
2021-11-21 17:27:28 -0500

    style + add sleeps to lower load on postgres

--- src/main/java/org/midnightbsd/advisory/services/NvdFetchService.java
+++ src/main/java/org/midnightbsd/advisory/services/NvdFetchService.java
@@ -1,6 +1,35 @@
+/*
+ * Copyright (c) 2017-2021 Lucas Holt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package org.midnightbsd.advisory.services;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -16,99 +45,97 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Date;
-
-/**
- * @author Lucas Holt
- */
+/** @author Lucas Holt */
 @Slf4j
 @Service
 public class NvdFetchService {
 
-    @Deprecated(forRemoval = true)
-    @Value("${nvdfeed.baseUrl}")
-    private String nvdfeedUrl;
+  /**
+   * @deprecated retiring legacy cve feeds
+   */
+  @Deprecated(forRemoval = true)
+  @Value("${nvdfeed.baseUrl}")
+  private String nvdfeedUrl;
 
-    @Value("${nvdfeed.serviceUrl}")
-    private String nvdServiceUrl;
+  @Value("${nvdfeed.serviceUrl}")
+  private String nvdServiceUrl;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+  @Autowired private ObjectMapper objectMapper;
 
-    private RestTemplate restTemplate;
+  private RestTemplate restTemplate;
 
-    @Autowired
-    public NvdFetchService(final RestTemplateBuilder builder) {
-        this.restTemplate = builder.build();
-    }
+  @Autowired
+  public NvdFetchService(final RestTemplateBuilder builder) {
+    this.restTemplate = builder.build();
+  }
 
-    /**
-     * In case we ever need to reload ALL data
-     * @param startIndex  record to start page with
-     * @return Single page of records
-     */
-    public CveDataPage getPage(final int startIndex) {
-        final String url = nvdServiceUrl + "cves/1.0?resultsPerPage=5000&startIndex={startIndex}";
-        return restTemplate.getForObject(url, CveDataPage.class, startIndex);
-    }
+  /**
+   * In case we ever need to reload ALL data
+   *
+   * @param startIndex record to start page with
+   * @return Single page of records
+   */
+  public CveDataPage getPage(final int startIndex) {
+    final String url = nvdServiceUrl + "cves/1.0?resultsPerPage=5000&startIndex={startIndex}";
+    return restTemplate.getForObject(url, CveDataPage.class, startIndex);
+  }
 
-    /**
-     * Reload data since a specific date
-     * @param modStartDate CVE modified start date
-     * @param startIndex record to start page with
-     * @return Single page of records
-     */                                                                  
-    public CveDataPage getPage(final Date modStartDate, final int startIndex) {
-        final String url = nvdServiceUrl + "cves/1.0?resultsPerPage=5000&startIndex={startIndex}&modStartDate={modStartDate}";
-        return restTemplate.getForObject(url, CveDataPage.class, startIndex, DateUtil.formatCveApiDate(modStartDate));
-    }
+  /**
+   * Reload data since a specific date
+   *
+   * @param modStartDate CVE modified start date
+   * @param startIndex record to start page with
+   * @return Single page of records
+   */
+  public CveDataPage getPage(final Date modStartDate, final int startIndex) {
+    final String url =
+        nvdServiceUrl
+            + "cves/1.0?resultsPerPage=5000&startIndex={startIndex}&modStartDate={modStartDate}";
+    return restTemplate.getForObject(
+        url, CveDataPage.class, startIndex, DateUtil.formatCveApiDate(modStartDate));
+  }
 
-    /**
-     * @deprecated CVE feeds retired
-     */
-    @Deprecated(forRemoval = true)
-    public CveData getNVDData(final String suffix) {
-        final String url = nvdfeedUrl + suffix;
+  /** @deprecated CVE feeds retired */
+  @Deprecated(forRemoval = true)
+  public CveData getNVDData(final String suffix) {
+    final String url = nvdfeedUrl + suffix;
 
-        log.info("Fetching nvd data for {}", url);
-        //  https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.gz
+    log.info("Fetching nvd data for {}", url);
+    //  https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.gz
 
-        try {
-            final HttpClient client = HttpClientBuilder.create().build();
-            final HttpGet request = new HttpGet(url);
+    try {
+      final HttpClient client = HttpClientBuilder.create().build();
+      final HttpGet request = new HttpGet(url);
 
-            final HttpResponse response = client.execute(request);
+      final HttpResponse response = client.execute(request);
 
-            if (response.getStatusLine().getStatusCode() != 200) {
-                log.error("Unable to fetch with status code {}",
-                        response.getStatusLine().getStatusCode());
-                return null;
-            }
-
-            final org.apache.http.HttpEntity entity = response.getEntity();
-            if (entity != null) {
-
-                final String contentType = entity.getContentType().getValue();
-                log.info("Content type is {}", contentType);
-
-                if (contentType.equalsIgnoreCase(" text/html")) {
-                    log.error("An error occurred and we got a web page");
-                    return null;
-                }
-
-                final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                entity.writeTo(baos);
-
-                final String decompressed = CompressUtil.extract(contentType, baos.toByteArray());
-                baos.close();
-
-                return objectMapper.readValue(decompressed, CveData.class);
-            }
-        } catch (final IOException e) {
-            log.error("network call failed.", e);
-        }
+      if (response.getStatusLine().getStatusCode() != 200) {
+        log.error("Unable to fetch with status code {}", response.getStatusLine().getStatusCode());
         return null;
+      }
+
+      final org.apache.http.HttpEntity entity = response.getEntity();
+      if (entity != null) {
+
+        final String contentType = entity.getContentType().getValue();
+        log.info("Content type is {}", contentType);
+
+        if (contentType.equalsIgnoreCase(" text/html")) {
+          log.error("An error occurred and we got a web page");
+          return null;
+        }
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        entity.writeTo(baos);
+
+        final String decompressed = CompressUtil.extract(contentType, baos.toByteArray());
+        baos.close();
+
+        return objectMapper.readValue(decompressed, CveData.class);
+      }
+    } catch (final IOException e) {
+      log.error("network call failed.", e);
     }
+    return null;
+  }
 }
--- src/main/java/org/midnightbsd/advisory/services/NvdImportService.java
+++ src/main/java/org/midnightbsd/advisory/services/NvdImportService.java
@@ -1,5 +1,36 @@
+/*
+ * Copyright (c) 2017-2021 Lucas Holt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package org.midnightbsd.advisory.services;
 
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.midnightbsd.advisory.model.Advisory;
 import org.midnightbsd.advisory.model.ConfigNode;
@@ -26,203 +57,201 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Set;
-
-/**
- * @author Lucas Holt
- */
+/** @author Lucas Holt */
 @Slf4j
 @Service
 public class NvdImportService {
 
-    @Autowired
-    private AdvisoryService advisoryService;
+  @Autowired private AdvisoryService advisoryService;
 
-    @Autowired
-    private VendorRepository vendorRepository;
+  @Autowired private VendorRepository vendorRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
+  @Autowired private ProductRepository productRepository;
 
-    @Autowired
-    private ConfigNodeRepository configNodeRepository;
+  @Autowired private ConfigNodeRepository configNodeRepository;
 
-    @Autowired
-    private ConfigNodeCpeRepository configNodeCpeRepository;
+  @Autowired private ConfigNodeCpeRepository configNodeCpeRepository;
 
-    private String getProblemType(final Cve cve) {
-        final StringBuilder sb = new StringBuilder();
-        for (final ProblemTypeData ptd : cve.getProblemType().getProblemTypeData()) {
-            for (final ProblemTypeDataDescription dd : ptd.getDescription()) {
-                sb.append(dd.getValue()).append(",");
+  private String getProblemType(final Cve cve) {
+    final StringBuilder sb = new StringBuilder();
+    for (final ProblemTypeData ptd : cve.getProblemType().getProblemTypeData()) {
+      for (final ProblemTypeDataDescription dd : ptd.getDescription()) {
+        sb.append(dd.getValue()).append(",");
+      }
+    }
+    return sb.toString();
+  }
+
+  private Vendor createOrFetchVendor(final VendorData vendorData) {
+    Vendor v = vendorRepository.findOneByName(vendorData.getVendorName());
+    if (v == null) {
+      v = new Vendor();
+      v.setName(vendorData.getVendorName());
+      v = vendorRepository.saveAndFlush(v);
+    }
+    return v;
+  }
+
+  private Product createOrFetchProduct(final ProductData pd, final VersionData vd, final Vendor v) {
+    Product product =
+        productRepository.findByNameAndVersionAndVendor(
+            pd.getProductName(), vd.getVersionValue(), v);
+    if (product == null) {
+      product = new Product();
+      product.setName(pd.getProductName());
+      product.setVersion(vd.getVersionValue());
+      product.setVendor(v);
+      product = productRepository.saveAndFlush(product);
+    }
+    return product;
+  }
+
+  private Set<Product> processVendorAndProducts(final Cve cve) {
+    final Set<Product> advProducts = new HashSet<>();
+    if (cve.getAffects() == null || cve.getAffects().getVendor() == null) return advProducts;
+
+    log.info("Vendor count: {}", cve.getAffects().getVendor().getVendorData().size());
+
+    for (final VendorData vendorData : cve.getAffects().getVendor().getVendorData()) {
+      final Vendor v = createOrFetchVendor(vendorData);
+
+      log.info("Product count {}", vendorData.getProduct().getProductData().size());
+      for (final ProductData pd : vendorData.getProduct().getProductData()) {
+        for (final VersionData vd : pd.getVersion().getVersionData()) {
+          advProducts.add(createOrFetchProduct(pd, vd, v));
+        }
+      }
+    }
+
+    return advProducts;
+  }
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public void importNvd(final CveData cveData) {
+    if (cveData == null) throw new IllegalArgumentException("cveData");
+
+    if (cveData.getCveItems() == null || cveData.getCveItems().isEmpty())
+      throw new IllegalArgumentException("cveData.getItems()");
+
+    for (final CveItem cveItem : cveData.getCveItems()) {
+      final Cve cve = cveItem.getCve();
+      Advisory advisory = new Advisory();
+
+      if (cve.getCveDataMeta() == null) {
+        log.warn("invalid metadata");
+        continue;
+      }
+
+      advisory.setCveId(cve.getCveDataMeta().getID());
+      log.info("Processing {}", advisory.getCveId());
+
+      if (cve.getProblemType() != null && cve.getProblemType().getProblemTypeData() != null) {
+        advisory.setProblemType(getProblemType(cve));
+      }
+
+      advisory.setPublishedDate(convertDate(cveItem.getPublishedDate()));
+      advisory.setLastModifiedDate(convertDate(cveItem.getLastModifiedDate()));
+
+      if (cve.getDescription() != null && cve.getDescription().getDescriptionData() != null) {
+        for (final DescriptionData descriptionData : cve.getDescription().getDescriptionData()) {
+          if (descriptionData.getLang().equalsIgnoreCase("en"))
+            advisory.setDescription(descriptionData.getValue());
+        }
+      }
+
+      // determine severity
+      if (cveItem.getImpact() != null && cveItem.getImpact().getBaseMetricV2() != null) {
+        advisory.setSeverity(cveItem.getImpact().getBaseMetricV2().getSeverity());
+      }
+
+      advisory.setProducts(processVendorAndProducts(cve));
+      advisory = advisoryService.save(advisory);
+
+      // now save configurations
+      if (cveItem.getConfigurations() != null && cveItem.getConfigurations().getNodes() != null) {
+        log.info("Now save configurations for {}", advisory.getCveId());
+        for (final Node node : cveItem.getConfigurations().getNodes()) {
+          if (node.getOperator() != null) {
+            ConfigNode configNode = new ConfigNode();
+            configNode.setAdvisory(advisory);
+            configNode.setOperator(node.getOperator());
+
+            configNode = configNodeRepository.save(configNode); // save top level item
+
+            if (node.getCpe() != null) {
+              for (final NodeCpe nodeCpe : node.getCpe()) {
+                final ConfigNodeCpe cpe = new ConfigNodeCpe();
+
+                cpe.setCpe22Uri(nodeCpe.getCpe22Uri());
+                cpe.setCpe23Uri(nodeCpe.getCpe23Uri());
+                cpe.setVulnerable(nodeCpe.getVulnerable());
+                cpe.setConfigNode(configNode);
+
+                configNodeCpeRepository.save(cpe);
+              }
             }
-        }
-        return sb.toString();
-    }
 
-    private Vendor createOrFetchVendor(final VendorData vendorData) {
-        Vendor v = vendorRepository.findOneByName(vendorData.getVendorName());
-        if (v == null) {
-            v = new Vendor();
-            v.setName(vendorData.getVendorName());
-            v = vendorRepository.saveAndFlush(v);
-        }
-        return v;
-    }
+            if (node.getChildren() != null) {
+              for (final Node childNode : node.getChildren()) {
+                if (childNode.getOperator() != null) {
+                  final ConfigNode cn = new ConfigNode();
+                  cn.setAdvisory(advisory);
+                  cn.setOperator(node.getOperator());
+                  cn.setParentId(configNode.getId());
+                  configNodeRepository.save(cn);
 
-    private Product createOrFetchProduct(final ProductData pd, final VersionData vd, final Vendor v) {
-        Product product = productRepository.findByNameAndVersionAndVendor(pd.getProductName(), vd.getVersionValue(), v);
-        if (product == null) {
-            product = new Product();
-            product.setName(pd.getProductName());
-            product.setVersion(vd.getVersionValue());
-            product.setVendor(v);
-            product = productRepository.saveAndFlush(product);
-        }
-        return product;
-    }
+                  if (childNode.getCpe() != null) {
+                    for (final NodeCpe nodeCpe : childNode.getCpe()) {
+                      final ConfigNodeCpe cpe = new ConfigNodeCpe();
 
-    private Set<Product> processVendorAndProducts(final Cve cve) {
-        final Set<Product> advProducts = new HashSet<>();
-        if (cve.getAffects() == null || cve.getAffects().getVendor() == null)
-            return advProducts;
+                      cpe.setCpe22Uri(nodeCpe.getCpe22Uri());
+                      cpe.setCpe23Uri(nodeCpe.getCpe23Uri());
+                      cpe.setVulnerable(nodeCpe.getVulnerable());
+                      cpe.setConfigNode(cn);
 
-        log.info("Vendor count: {}", cve.getAffects().getVendor().getVendorData().size());
+                      configNodeCpeRepository.save(cpe);
+                    }
+                  }
 
-        for (final VendorData vendorData : cve.getAffects().getVendor().getVendorData()) {
-            final Vendor v = createOrFetchVendor(vendorData);
+                  // currently, do not support child-child nodes.
 
-            log.info("Product count {}", vendorData.getProduct().getProductData().size());
-            for (final ProductData pd : vendorData.getProduct().getProductData()) {
-                for (final VersionData vd : pd.getVersion().getVersionData()) {
-                    advProducts.add(createOrFetchProduct(pd, vd, v));
+                  try {
+                    Thread.sleep(500L);
+                  } catch (InterruptedException e) {
+                    log.error("Issue sleeping during nvd import", e);
+                  }
                 }
+              }
             }
+          }
         }
 
-        return advProducts;
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void importNvd(final CveData cveData) {
-        if (cveData == null)
-            throw new IllegalArgumentException("cveData");
-
-        if (cveData.getCveItems() == null || cveData.getCveItems().isEmpty())
-            throw new IllegalArgumentException("cveData.getItems()");
-
-        for (final CveItem cveItem : cveData.getCveItems()) {
-            final Cve cve = cveItem.getCve();
-            Advisory advisory = new Advisory();
-
-            if (cve.getCveDataMeta() == null) {
-                log.warn("invalid metadata");
-                continue;
-            }
-
-            advisory.setCveId(cve.getCveDataMeta().getID());
-            log.info("Processing {}", advisory.getCveId());
-
-            if (cve.getProblemType() != null && cve.getProblemType().getProblemTypeData() != null)  {
-                advisory.setProblemType(getProblemType(cve));
-            }
-
-            advisory.setPublishedDate(convertDate(cveItem.getPublishedDate()));
-            advisory.setLastModifiedDate(convertDate(cveItem.getLastModifiedDate()));
-
-            if (cve.getDescription() != null && cve.getDescription().getDescriptionData() != null) {
-                for(final DescriptionData descriptionData : cve.getDescription().getDescriptionData()) {
-                   if (descriptionData.getLang().equalsIgnoreCase("en"))
-                        advisory.setDescription(descriptionData.getValue());
-                }
-            }
-
-            // determine severity
-            if (cveItem.getImpact() != null && cveItem.getImpact().getBaseMetricV2() != null) {
-                advisory.setSeverity(cveItem.getImpact().getBaseMetricV2().getSeverity());
-            }
-            
-            advisory.setProducts(processVendorAndProducts(cve));
-            advisory = advisoryService.save(advisory);
-
-            // now save configurations
-            if (cveItem.getConfigurations() != null && cveItem.getConfigurations().getNodes() != null) {
-                log.info("Now save configurations for {}", advisory.getCveId());
-                for (final Node node : cveItem.getConfigurations().getNodes()) {
-                     if (node.getOperator() != null) {
-                         ConfigNode configNode = new ConfigNode();
-                         configNode.setAdvisory(advisory);
-                         configNode.setOperator(node.getOperator());
-
-                         configNode = configNodeRepository.save(configNode); // save top level item
-
-                         if (node.getCpe() != null) {
-                             for (final NodeCpe nodeCpe : node.getCpe()) {
-                                 final ConfigNodeCpe cpe = new ConfigNodeCpe();
-
-                                 cpe.setCpe22Uri(nodeCpe.getCpe22Uri());
-                                 cpe.setCpe23Uri(nodeCpe.getCpe23Uri());
-                                 cpe.setVulnerable(nodeCpe.getVulnerable());
-                                 cpe.setConfigNode(configNode);
-
-                                 configNodeCpeRepository.save(cpe);
-                             }
-                         }
-
-                         if (node.getChildren() != null) {
-                             for (final Node childNode : node.getChildren()) {
-                                 if (childNode.getOperator() != null) {
-                                     final ConfigNode cn = new ConfigNode();
-                                     cn.setAdvisory(advisory);
-                                     cn.setOperator(node.getOperator());
-                                     cn.setParentId(configNode.getId());
-                                     configNodeRepository.save(cn);
-
-                                     if (childNode.getCpe() != null) {
-                                         for (final NodeCpe nodeCpe : childNode.getCpe()) {
-                                             final ConfigNodeCpe cpe = new ConfigNodeCpe();
-
-                                             cpe.setCpe22Uri(nodeCpe.getCpe22Uri());
-                                             cpe.setCpe23Uri(nodeCpe.getCpe23Uri());
-                                             cpe.setVulnerable(nodeCpe.getVulnerable());
-                                             cpe.setConfigNode(cn);
-
-                                             configNodeCpeRepository.save(cpe);
-                                         }
-                                     }
-
-                                    // currently do not support child child nodes.
-                                 }
-                             }
-                         }
-
-                     }
-                }
-
-                configNodeRepository.flush();
-                configNodeCpeRepository.flush();
-            }
-        }
-    }
-
-    private Date convertDate(final String dt) {
-        if (dt == null || dt.isEmpty())
-            return null;
-
-        // 2018-02-20T21:29Z
+        configNodeRepository.flush();
+        configNodeCpeRepository.flush();
 
         try {
-            // deepcode ignore FixDateFormat: API we're calling doesn't output in JS date format
-            final SimpleDateFormat iso8601Dateformat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'", Locale.US);
-            return iso8601Dateformat.parse(dt);
-        } catch (final Exception e) {
-            log.error("Could not convert date string {}", dt, e);
+          Thread.sleep(200L);
+        } catch (InterruptedException e) {
+          log.error("Issue sleeping during nvd import", e);
         }
-
-        return null;
+      }
     }
+  }
+
+  private Date convertDate(final String dt) {
+    if (dt == null || dt.isEmpty()) return null;
+
+    // 2018-02-20T21:29Z
+
+    try {
+      // deepcode ignore FixDateFormat: API we're calling doesn't output in JS date format
+      final SimpleDateFormat iso8601Dateformat =
+          new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'", Locale.US);
+      return iso8601Dateformat.parse(dt);
+    } catch (final Exception e) {
+      log.error("Could not convert date string {}", dt, e);
+    }
+
+    return null;
+  }
 }
