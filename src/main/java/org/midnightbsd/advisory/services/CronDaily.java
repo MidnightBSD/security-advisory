4ec336f77173d28bf7de9db8b2dae2c8497cc407
Lucas Holt <luke@foolishgames.com>
2021-11-21 17:30:36 -0500

    update spring boot.

--- src/main/java/org/midnightbsd/advisory/services/CronDaily.java
+++ src/main/java/org/midnightbsd/advisory/services/CronDaily.java
@@ -1,38 +1,58 @@
+/*
+ * Copyright (c) 2017-2021 Lucas Holt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package org.midnightbsd.advisory.services;
 
+
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.midnightbsd.advisory.model.nvd.CveData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
-/**
- * @author Lucas Holt
- */
+/** @author Lucas Holt */
 @Slf4j
 @Service
 public class CronDaily {
-    private static final int DELAY_ONE_MINUTE = 1000 * 60;
-    private static final int ONE_DAY = DELAY_ONE_MINUTE * 60 * 24;
-    private static final int TEN_MINUTES = DELAY_ONE_MINUTE * 10;
-    private static final String RECENT_SUFFIX = "nvdcve-1.0-recent.json.gz";
+  private static final int DELAY_ONE_MINUTE = 1000 * 60;
+  private static final int ONE_DAY = DELAY_ONE_MINUTE * 60 * 24;
+  private static final int TEN_MINUTES = DELAY_ONE_MINUTE * 10;
+  private static final String RECENT_SUFFIX = "nvdcve-1.0-recent.json.gz";
 
-    @Autowired
-    private NvdFetchService nvdFetchService;
+  @Autowired private NvdFetchService nvdFetchService;
 
-    @Autowired
-    private NvdImportService nvdImportService;
+  @Autowired private NvdImportService nvdImportService;
 
-    @Scheduled(fixedDelay = ONE_DAY, initialDelay = TEN_MINUTES)
-    public void daily() throws IOException {
-        final CveData recent = nvdFetchService.getNVDData(RECENT_SUFFIX);
+  @Scheduled(fixedDelay = ONE_DAY, initialDelay = TEN_MINUTES)
+  public void daily() throws IOException {
+    final CveData recent = nvdFetchService.getNVDData(RECENT_SUFFIX);
 
-        log.info("Begin import of recent data");
-        nvdImportService.importNvd(recent);
+    log.info("Begin import of recent data");
+    nvdImportService.importNvd(recent);
 
-        log.info("Finished daily import");
-    }
-
+    log.info("Finished daily import");
+  }
 }
