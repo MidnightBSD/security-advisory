6ba1fb0cb1bd8ef8d276a136019576f4f628af1b
Lucas Holt <luke@foolishgames.com>
2023-04-16 23:52:28 -0400

    fix some deprecated code

--- src/main/java/org/midnightbsd/advisory/util/CompressUtil.java
+++ src/main/java/org/midnightbsd/advisory/util/CompressUtil.java
@@ -25,19 +25,21 @@
  */
 package org.midnightbsd.advisory.util;
 
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.zip.DeflaterInputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipException;
 import java.util.zip.ZipInputStream;
-import org.apache.commons.codec.Charsets;
 import org.apache.commons.compress.utils.IOUtils;
 
 /** @author Lucas Holt */
 public class CompressUtil {
+
+  private CompressUtil() {}
+
   public static byte[] decompressGzip(final byte[] contentBytes) throws IOException {
     try (final ByteArrayOutputStream out = new ByteArrayOutputStream()) {
       IOUtils.copy(new GZIPInputStream(new ByteArrayInputStream(contentBytes)), out);
@@ -62,30 +64,30 @@ public class CompressUtil {
   public static String extract(String contentType, byte[] responseBytes) throws IOException {
     String decompressed;
     try {
-      decompressed = new String(decompressGzip(responseBytes), Charsets.UTF_8);
+      decompressed = new String(decompressGzip(responseBytes), StandardCharsets.UTF_8);
     } catch (final ZipException zip) {
       // fallback to raw string
-      decompressed = new String(responseBytes, Charsets.UTF_8);
+      decompressed = new String(responseBytes, StandardCharsets.UTF_8);
     }
 
     // try zip if gzip fails
     if (contentType.equalsIgnoreCase("application/x-zip")
         || contentType.equalsIgnoreCase("application/zip")) {
       try {
-        decompressed = new String(decompressZip(responseBytes), Charsets.UTF_8);
+        decompressed = new String(decompressZip(responseBytes), StandardCharsets.UTF_8);
       } catch (final ZipException ignored) {
         // fallback to raw string
-        decompressed = new String(responseBytes, Charsets.UTF_8);
+        decompressed = new String(responseBytes, StandardCharsets.UTF_8);
       }
     }
 
     if (contentType.equalsIgnoreCase("application/x-deflate")
         || contentType.equalsIgnoreCase("application/deflate")) {
       try {
-        decompressed = new String(decompressDeflate(responseBytes), Charsets.UTF_8);
+        decompressed = new String(decompressDeflate(responseBytes), StandardCharsets.UTF_8);
       } catch (final ZipException z3) {
         // fallback to raw string
-        decompressed = new String(responseBytes, Charsets.UTF_8);
+        decompressed = new String(responseBytes, StandardCharsets.UTF_8);
       }
     }
 
